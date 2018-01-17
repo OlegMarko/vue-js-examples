e6896618200fa093f53e40fb3b71d4de971672cf
OlegMarko <olegmarko7@gmail.com>
2018-01-18 00:53:59 +0200

    ex 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello World'
+        title: 'Hello World',
+        title2: ''
+    },
+    methods: {
+        changeText () {
+            this.title = 'Text';
+        },
+        inputText (event) {
+            this.title2 = event.target.value;
+        }
     }
 });
