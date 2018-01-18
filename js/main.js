8da68d67b552d77026b27d7711c79d60e485fb70
OlegMarko <olegmarko7@gmail.com>
2018-01-18 13:52:06 +0200

    ex 5

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
+Vue.filter('capitalize', function (val) {
+    if (!val) return '';
+    val = val.toString();
+    return val.replace(/\b\w/g, function (l) { return l.toUpperCase() });
+});
+
 new Vue({
     el: '#app',
     data: {
         show: true,
+        message: 'message (v-else)',
         cars: [
-            {model: "BMW", speed: 250.8},
-            {model: "Audi", speed: 240.55},
-            {model: "Mercedes-Benz AMG", speed: 350.05},
-            {model: "Ford", speed: 160.75}
+            {model: "bMW", speed: 250.8},
+            {model: "audi", speed: 240.55},
+            {model: "mercedes-benz AMG", speed: 350.05},
+            {model: "ford", speed: 160.75}
         ]
     },
     methods: {
@@ -15,6 +22,13 @@ new Vue({
         }
     },
     computed: {
-        //
+        showMessage () {
+            return this.message.toUpperCase();
+        }
+    },
+    filters: {
+        lowercase (val) {
+            return val.toLowerCase();
+        }
     }
 });
