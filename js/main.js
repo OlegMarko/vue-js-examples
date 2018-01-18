67e70bb3f4ff8388065fdf5ee51ca2499f4e621e
OlegMarko <olegmarko7@gmail.com>
2018-01-18 20:10:27 +0200

    ex 6

--- js/main.js
+++ js/main.js
@@ -1,34 +1,31 @@
-Vue.filter('capitalize', function (val) {
-    if (!val) return '';
-    val = val.toString();
-    return val.replace(/\b\w/g, function (l) { return l.toUpperCase() });
+Vue.component('cars', {
+    data: function () {
+        return {
+            cars: [
+                {model: "BMW"},
+                {model: "Audi"},
+                {model: "Mercedes-Benz AMG"},
+                {model: "Ford"},
+                {model: "Volvo"}
+            ]
+        };
+    },
+    template: '<div><div class="car" v-for="car in cars"><p>{{ car.model }}</p></div></div>'
 });
+
 
 new Vue({
     el: '#app',
     data: {
-        show: true,
-        message: 'message (v-else)',
-        cars: [
-            {model: "bMW", speed: 250.8},
-            {model: "audi", speed: 240.55},
-            {model: "mercedes-benz AMG", speed: 350.05},
-            {model: "ford", speed: 160.75}
-        ]
+        //
     },
     methods: {
-        toggleText () {
-            this.show = !this.show
-        }
+        //
     },
     computed: {
-        showMessage () {
-            return this.message.toUpperCase();
-        }
+        //
     },
     filters: {
-        lowercase (val) {
-            return val.toLowerCase();
-        }
+        //
     }
 });
